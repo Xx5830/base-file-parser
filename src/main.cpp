28970e31d2d0fb1c4a30cd3038802f0f6f7c7e0d
X_xp <timurbilalov14@gmail.com>
2025-09-26 14:09:36 +0300

    add ArrayList (class Itarator don't work)

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,82 @@
 #include <cstdint>
 #include <iostream>
 
-namespace cont {
+namespace contain {
+template <typename T>
+class Collection;
+
 template <typename T>
 class Iterator {
-  virtual bool HaveNext();
-  virtual void Next();
-  virtual T get();
+ public:
+  virtual bool HasNext() = 0;
+  virtual void Next() = 0;
+  virtual const T* Get() = 0;
 };
 
+// Collection interface for iterable
 template <typename T>
 class Collection {
  public:
-  uint32_t size;
-
-  Collection(uint32_t _size) { size = _size; }
-  virtual Iterator<T> begin() = 0;
-
-  virtual T get(uint32_t index) = 0;
-
-  virtual void set(uint32_t index, T value) = 0;
-
-  virtual void insert(uint32_t index) = 0;
-
-  virtual void erase(uint32_t index) = 0;
-  virtual uint32_t length() {return size;};
+  virtual const T* Get(uint32_t index) = 0;
+  virtual void Set(uint32_t index, const T& value) = 0;
+  virtual Iterator<T>* GetIterator() = 0;
+  virtual uint32_t Length() = 0;
 };
 
+// Dynamic list with fix length
 template <typename T>
+class ArrayList : public Collection<T> {
+ private:
+  T* arr;
+  uint32_t size;
+
+  template <typename U>
+  class MyIterator : public Iterator<U> {
+   private:
+    const ArrayList<T>* kArrayList;
+    uint32_t index;
+
+   public:
+    MyIterator(const ArrayList<T>*& kArrayList) {
+      this->kArrayList = &kArrayList;
+    }
+
+    bool HasNext() { return index + 1 < kArrayList->Length(); }
+    void Next() { ++index; }
+    const T* Get() { return kArrayList->Get(index); }
+    ~MyIterator() { delete index; }
+  };
+
+ public:
+  ArrayList(uint32_t _size) {
+    size = _size;
+
+    arr = new T[Length()];
+  }
+  ~ArrayList() { delete[] arr; }
+
+  const T* Get(uint32_t index) {
+    if (index >= Length()) {
+      // need return error and log info
+    }
+
+    return &arr[index];
+  }
+
+  void Set(uint32_t index, const T& value) {
+    if (index >= Length()) {
+      // need return error and log info
+    }
+
+    arr[index] = value;
+  }
+
+  Iterator<T>* GetIterator() { return new MyIterator<T>(this); }
+
+  uint32_t Length() { return size; }
+};
+
+/* template <typename T>
 class List : public Collection<T> {
  private:
   T* arr;
@@ -47,8 +96,20 @@ class List : public Collection<T> {
   T get(uint32_t index) {
     return arr[index];
   }
-};
 
-};  // namespace cont
+  template <typename T>
+  class Vector : public : List{
 
-int main(int, char**) { cont::List<int> mas(5, 1); }
+  };
+}; */
+
+};  // namespace contain
+
+int main(int, char**) {
+  contain::ArrayList<int> mas(5);
+
+  for (int i = 0; i < 5; i++) {
+    mas.Set(i, i + 1);
+    std::cout << (*mas.Get(i)) << "\n";
+  }
+}
